b0bcb8da36527537986c6af9f9f9bae93fe944e5
Hampus Karlsson <hampus04.karlsson@gmail.com>
2025-05-26 11:25:54 +0200

    Added code to gather job ads

--- load_job_ads.py
+++ load_job_ads.py
@@ -10,3 +10,14 @@ pipeline =dlt.pipeline(
     dataset_name="raw_data"
 )
 
+# Gather ads
+def get_job_ads(occupation_field: str, pages: int = 3):
+    all_results = []
+    for page in range(1, pages + 1):
+        response = requests.get(
+            "https://jobsearch.api.jobtechdev.se/search",
+            params={"occupation-field": occupation_field, "page": page}
+        )
+        data = response.json()
+        all_results.exted(data.get("hits",[]))
+    return all_results
