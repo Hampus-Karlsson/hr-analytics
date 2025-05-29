fc6999f7edd70eefb379fd165031c60a8fbed90d
Hampus Karlsson <hampus04.karlsson@gmail.com>
2025-05-29 10:59:01 +0200

    starting a revamp

--- load_job_ads.py
+++ load_job_ads.py
@@ -1,14 +1,33 @@
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 import requests
 import dlt
 
-# Hämta yrkesfält (occupation fields)
-def get_occupation_fields():
-    url = "https://jobsearch.api.jobtechdev.se/metadata/occupationfields"
-    headers = {"Accept": "application/json;version=2"}
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    return response.json()
 
+# Hämta yrkesfält (occupation fields) från graphQL
+def get_occupation_fields_from_taxonomy():
+    transport = RequestsHTTPTransport(
+    url = "https://taxonomy.api.jobtechdev.se/v1/graphql",
+    headers = {"Accept": "application/json"}
+    verify=True,
+    retries=3,
+    )
+
+    client =Client(transport=transport, fetch_schema_from_transport=True)
+
+    query = gql("""
+        query {
+            concepts(type:"occupation-field"){
+                id
+                preferred_label
+                }
+            }
+    """)
+
+    result = client.execute(query)
+    return result["concepts"]
+
+##anonshämtning
 def get_job_ads(occupation_field_id, page=0, size=100):
     url = "https://jobsearch.api.jobtechdev.se/search"
     headers = {"Accept": "application/json;version=2"}
@@ -22,33 +41,20 @@ def get_job_ads(occupation_field_id, page=0, size=100):
     data = response.json()
     return data.get("results", [])
 
-def ads_resource(occupation_field_id):
-    ads = get_job_ads(occupation_field_id)
+#dlt
+def ads_resource(ads):
     for ad in ads:
         yield ad
-#koddel
 
+#Main
 if __name__ == "__main__":
-    fields = get_occupation_fields()
-    # Hitta några yrkesfält id:n (exempel)
+    fields = get_occupation_fields_from_taxonomy()
+    
     print("Några yrkesfält:")
-    for f in fields:
-        print(f"{f['id']}, Namn: {f['label']['sv']}")
+   
+   data_it_id =next((f['id'] for f in fields if "data" in f['preferred_label']))
 
-    # Ta id för t.ex. "Data-IT" eller liknande från utskriften
-    data_it_id = None
-    bygg_id = None
-    halsa_id = None
-
-    for f in fields:
-        if "data" in f['label']['sv'].lower():
-            data_it_id = f['id']
-        elif "bygg" in f['label']['sv'].lower():
-            bygg_id = f['id']
-        elif "hälso" in f['label']['sv'].lower():
-            halsa_id = f['id']
-
-    print(f"Data IT id: {data_it_id}")
+    print(f"Data/IT id: {data_it_id}")
     print(f"Bygg id: {bygg_id}")
     print(f"Hälsa id: {halsa_id}")
 
